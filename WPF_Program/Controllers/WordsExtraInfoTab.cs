6015c9b5b431bae88e411399f862d71d6c51c5ad
Traian Chiș <chisioantraian@outlook.com>
2020-03-25 20:19:10 +0200

    better decomposition of char into components

--- WPF_Program/Controllers/WordsExtraInfoTab.cs
+++ WPF_Program/Controllers/WordsExtraInfoTab.cs
@@ -1,7 +1,10 @@
 ﻿using ChineseAppWPF.Logic;
+using ChineseAppWPF.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Windows.Controls;
 
 namespace ChineseAppWPF.Controllers
 {
@@ -9,8 +12,43 @@ namespace ChineseAppWPF.Controllers
     {
         private static void ShowCharacterDecomposition(char characterToBeDecomposed)
         {
-            string decompositionText = Decomposition.DecomposeCharToRadicals(characterToBeDecomposed);
-            mainWindow.DecompositionBlock.Text = $"{decompositionText} ";
+            //string decompositionText = Decomposition.DecomposeCharToRadicals(characterToBeDecomposed);
+            //mainWindow.DecompositionBlock.Text = $"{decompositionText} ";
+
+            //mainWindow.DecompositionWordList.Items.Clear();
+
+            List<string> chars = Decomposition.DecomposeCharToRadicals_2(characterToBeDecomposed);
+            List<Word> words = ChineseService.GetAllWordsFrom(chars).ToList();
+
+            //foreach (Word w in words)
+            //{
+            //    ListBoxItem item = new ListBoxItem { Content = $"{w.Simplified} , {w.Pinyin}", FontSize = 20 };
+            //    mainWindow.DecompositionWordList.Items.Add(item);
+            //}
+
+            UpdateShownWords_Left(words);
+
+        }
+
+        internal static void UpdateShownWords_Left(this IEnumerable<Word> filteredWords)
+        {
+            static SPPair makeSPP(char chn, string pron) => new SPPair { ChineseCharacter = chn, Pinyin = pron };
+
+            static ResultWord ResultedWordFromWord(Word word)
+            {
+                IEnumerable<char> singleChar = word.Simplified;
+                IEnumerable<string> singlePron = word.Pinyin.Split(" ");
+                IEnumerable<SPPair> sPPairs = singleChar.Zip(singlePron, makeSPP);
+
+                return new ResultWord
+                {
+                    SimplifiedPinyinPairs = sPPairs,
+                    Definitions = word.Definitions
+                };
+            }
+
+            mainWindow.DecompositionWordList.ItemsSource = filteredWords.Select(ResultedWordFromWord);
+            //mainWindow.WordsList.ItemsSource = filteredWords.Select(ResultedWordFromWord);
         }
     }
 }
--- WPF_Program/Logic/Decomposition.cs
+++ WPF_Program/Logic/Decomposition.cs
@@ -81,6 +81,56 @@ namespace ChineseAppWPF.Logic
             return decompositionText.ToString();
         }
 
+        public static List<string> DecomposeCharToRadicals_2(char topChar)
+        {
+            if (!basicDict.ContainsKey(topChar))
+                //return "cannot find decomposition";
+                return new List<string>();
+
+            if (Kangxi.CheckIfKangxiRadical(topChar))
+                //return topChar.ToString() + " - Kangxi Radical\n";
+                return new List<string> { topChar.ToString() };
+
+            List<string> result = new List<string> { topChar.ToString() };
+            //StringBuilder decompositionText = new StringBuilder();
+            Queue<char> chars = new Queue<char>();
+
+            foreach (char c in basicDict[topChar])
+            {
+                //decompositionText.Append(c.ToString());
+                chars.Enqueue(c);
+            }
+            //decompositionText.Append("\n");
+
+            while (chars.Count > 0)
+            {
+                char firstChar = chars.Dequeue();
+                //decompositionText.Append(firstChar.ToString()).Append(" : ");
+                result.Add(firstChar.ToString());
+                if (Kangxi.CheckIfKangxiRadical(firstChar))
+                {
+                    //decompositionText.Append(" - Kangxi Radical\n");
+                    ;
+                }
+                else if (basicDict.ContainsKey(firstChar))
+                {
+                    foreach (char c in basicDict[firstChar])
+                    {
+                        //decompositionText.Append(" ").Append(c.ToString());
+                        chars.Enqueue(c);
+                    }
+                    //decompositionText.Append("\n");
+                }
+                else
+                {
+                    //decompositionText.Append(" Stroke / Unencoded\n");
+                    ;
+                }
+            }
+            //return decompositionText.ToString();
+            return result;
+        }
+
         public static IEnumerable<Word> GetCharactersWithComponent(string text)
         {
             char ch = text[0];
